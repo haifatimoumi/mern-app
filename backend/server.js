350a90ca7bc8cf86169c7b227d7715af2c2e85e5
haifa <haifatimoumi6@gmail.com>
2026-06-13 22:01:05 +0100

    fix app initialization render

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.get("/products", async (req, res) => {
 // Frontend build (إذا تستعمل React داخل backend)
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
